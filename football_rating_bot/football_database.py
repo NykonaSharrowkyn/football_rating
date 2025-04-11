a11ba922e7a1a0df945650d140e19ae9be92d0e4
sharrowkyn <abolginov@gmail.com>
2025-04-11 16:56:02 +0300

    working player registration

--- football_rating_bot/football_database.py
+++ football_rating_bot/football_database.py
@@ -1,9 +1,13 @@
 import logging
 import uuid
 
-from sqlalchemy import create_engine, Column, UUID, String, Integer
+from functools import wraps
+
+from sqlalchemy import create_engine, Column, UUID, String, Integer, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session, relationship
+
+from typing import Callable, List
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -15,10 +19,39 @@ Base = declarative_base()
 
 class Event(Base):
     __tablename__ = 'events'
-    evnet_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    event_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     owner_id = Column(Integer)
-    message_id = Column(String)
-    event_name = Column(String)
+    event_title = Column(String)
+
+    players = relationship('EventPlayer', back_populates='event')
+    messages = relationship('Message', back_populates='event')
+
+class Player(Base):
+    __tablename__ = 'players'
+    player_id = Column(UUID(as_uuid=True), primary_key=True)
+    player_name = Column(String)
+    telegram_id = Column(Integer)
+    chat_id = Column(Integer, default=0)
+    elo = Column(Integer, default=1250)
+    matches = Column(Integer, default=0)
+
+    events = relationship('EventPlayer', back_populates='player')
+
+class Message(Base):
+    __tablename__ = 'messages'
+    inline_id = Column(UUID(as_uuid=True), primary_key=True)
+    message_id = Column(String, unique=True)    
+    event_id = Column(UUID(as_uuid=True), ForeignKey('events.event_id'))
+
+    event = relationship('Event', back_populates='messages')
+
+class EventPlayer(Base):
+    __tablename__ = 'event_players'
+    player_id = Column(UUID, ForeignKey('players.player_id'), primary_key=True)
+    event_id = Column(UUID, ForeignKey('events.event_id'), primary_key=True)
+    
+    event = relationship("Event", back_populates='players')
+    player = relationship("Player", back_populates='events')
 
 # class Player(Base):
 #     __tablename__ = 'players'
@@ -26,42 +59,79 @@ class Event(Base):
 #     user_id = Column(Integer)
 #     user
 
+def model_to_dict(model):
+    if model is None:
+        return None
+    return {column.name: getattr(model, column.name) for column in model.__table__.columns}
+
+def with_session(func: Callable):
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        with self.session() as session:
+            try:
+                return func(self, session, *args, **kwargs)
+            except Exception as e:
+                logger.debug(f'Ошибка в {func.__name__}: {str(e)}')
+                logger.debug(args)
+                logger.debug(kwargs)
+    return wrapper
+
+def with_commit(func: Callable):
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        with self.session() as session:
+            try:
+                result = func(self, session, *args, **kwargs)
+                session.commit()
+                return result
+            except Exception as e:
+                logger.debug(f'Ошибка в {func.__name__}: {str(e)}')
+                logger.debug(args)
+                logger.debug(kwargs)
+                session.rollback()
+    return wrapper
+
 class FootballDatabase:
     def __init__(self, db_path: str):
         self.engine = create_engine(db_path)
         Base.metadata.create_all(self.engine)
         self.session = sessionmaker(bind=self.engine)
 
-    def add_event(self, event_id: uuid.UUID, owner_id: str, message_id: str, event_name: str):
-        with self.session() as session:
-            logger.debug(f'Сохранение в базу: event_id = {event_id}, owner_id={owner_id}, message_id={message_id}')
-            try:
-                new_event = Event(
-                    evnet_id=event_id,
-                    owner_id=owner_id,
-                    message_id=message_id,
-                    event_name=event_name
-                )                
-                session.add(new_event)                
-                session.commit()
-                logger.debug(f'Commit')
-            except Exception as e:
-                session.rollback()
-                logger.debug(f'Rollback')
+    def add_event(self, event_id: uuid.UUID, owner_id: str, event_title: str) -> uuid.UUID | None:
+        return self._add_event(event_id, owner_id, event_title)    
+    
+    def add_event_message(self, inline_id: uuid.UUID, message_id: uuid.UUID, event_id: uuid.UUID) -> uuid.UUID | None:
+        return self._add_event_message(inline_id, message_id, event_id)
 
+    def add_player(self, name: str, telegram_id: int, elo: int) -> uuid.UUID | None:
+        return self._add_player(name, telegram_id, elo)
+    
     def get_event(self, event_id: uuid.UUID) -> dict | None:
-        with self.session() as session:
-            try:
-                logger.debug(f'Чтение из базы: event_id = {event_id}')
-                event = session.query(Event).filter(Event.evnet_id == event_id).first()
-                if not event:
-                    return None
-                event_dict = event._asdict()
-                return event_dict
-            except Exception as e:
-                pass
-            
-    def remove_event(self, event_id: uuid.UUID):
+        return self._get_event(event_id)
+   
+    def get_event_id(self, inline_id: uuid.UUID) -> uuid.UUID | None:
+        return self._get_event_id(inline_id)
+    
+    def get_messages(self, event_id: uuid.UUID) -> List[str] | None:
+        return self._get_messages(event_id)
+  
+    def get_player_id(self, telegram_id: int) -> uuid.UUID | None:
+        return self._get_player_id(telegram_id)
+    
+    def is_player_registered(self, player_id: uuid.UUID, event_id: uuid.UUID) -> bool:
+        return self._is_player_registered(player_id, event_id)
+    
+    # def is_player_registered(self, player_id, event_id) -> bool:
+    #     return self._is_player_registered(player_id, event_id)
+
+    def register_player(self, player_id: uuid.UUID, event_id: uuid.UUID) -> bool | None:
+        return self._register_player(player_id, event_id)
+    
+    def registered_players(self, event_id: uuid.UUID) -> bool | None:
+        return self._registered_players(event_id)
+    
+    def remove_event(self, session: Session, event_id: uuid.UUID):
+        raise NotImplementedError('delete messages from table')
         with self.session() as session:            
             try:
                 logger.debug(f'Удаление из базы: event_id = {event_id}')
@@ -74,6 +144,99 @@ class FootballDatabase:
             except Exception as e:
                 session.rollback()
                 logger.debug('Rollback')
+
+    def unregister_player(self, player_id: uuid.UUID, event_id: uuid.UUID) -> bool | None:                
+        return self._unregister_player(player_id, event_id)
+
+    # def update_event_text(self, event_id: uuid.UUID, new_text: str) -> None:
+    #     return self._update_event_text(event_id, new_text)    
+    
+    @with_commit
+    def _add_event(self, session: Session, event_id: uuid.UUID, owner_id: str, event_title: str) -> uuid.UUID | None:
+        new_event = Event(
+            event_id=event_id,
+            owner_id=owner_id,
+            event_title=event_title
+        )
+        session.add(new_event)
+        return event_id    
+    
+    @with_commit
+    def _add_event_message(self, session: Session, inline_id: uuid.UUID, message_id: uuid.UUID, event_id: uuid.UUID) -> uuid.UUID | None:
+        new_message = Message(inline_id=inline_id, message_id=message_id, event_id=event_id)
+        session.add(new_message)
+        return inline_id    
+    
+    @with_commit
+    def _add_player(self, session: Session, name: str, telegram_id: int, elo: int) -> uuid.UUID | None:
+        id = uuid.uuid4()
+        new_player = Player(
+            player_id=id,
+            player_name=name,
+            telegram_id=telegram_id,
+            elo=elo
+        )
+        session.add(new_player)
+        return id    
+    
+    @with_session
+    def _get_event(self, session: Session, event_id: uuid.UUID) -> dict | None:
+        event = session.query(Event).filter_by(event_id=event_id).first()
+        return model_to_dict(event)
+    
+    @with_session
+    def _get_event_id(self, session: Session, inline_id: uuid.UUID) -> uuid.UUID | None:
+        rec = session.query(Message).filter_by(inline_id=inline_id).first()
+        if not rec:
+            return None
+        return rec.event_id
+    
+    @with_session
+    def _get_messages(self, session: Session, event_id: uuid.UUID) -> List[str] | None:
+        event = session.query(Event).filter_by(event_id=event_id).first()        
+        return [message.message_id for message in event.messages]
+    
+    @with_session
+    def _get_player_id(self, session: Session, telegram_id: int) -> uuid.UUID | None:
+        rec = session.query(Player).filter_by(telegram_id=telegram_id).first()
+        if not rec:
+            return None
+        return rec.player_id            
+    
+    @with_session
+    def _is_player_registered(self, session: Session, player_id: uuid.UUID, event_id: uuid.UUID) -> bool:
+        rec = session.query(EventPlayer).filter_by(player_id=player_id, event_id=event_id).first()
+        return bool(rec)
+    
+    @with_commit
+    def _register_player(self, session: Session, player_id: uuid.UUID, event_id: uuid.UUID) -> bool | None:
+        link = EventPlayer(player_id=player_id, event_id=event_id)
+        session.add(link)
+        return True
+    
+    @with_session
+    def _registered_players(self, session: Session, event_id: uuid.UUID) -> List[dict] | None:
+        event = session.query(Event).filter_by(event_id=event_id).first()        
+        return [model_to_dict(player.player) for player in event.players]
+    
+    # @with_session
+    # def is_player_registered(self, session: Session, player_id, event_id) -> bool:
+    #     rec = session.query(EventPlayer).filter(
+    #         EventPlayer.event_id == event_id,
+    #         EventPlayer.player_id == player_id
+    #     ).first()
+    #     return bool(rec)
+
+    # @with_commit
+    # def _update_event_text(self, session: Session, event_id: uuid.UUID, new_text: str) -> None:
+    #     event = session.query(Event).filter(Event.event_id == event_id).first()
+    #     event.event_text = new_text
+    #     return new_text
+    @with_commit
+    def _unregister_player(self, session: Session, player_id: uuid.UUID, event_id: uuid.UUID) -> bool | None:                
+        link = session.query(EventPlayer).filter_by(player_id=player_id, event_id=event_id).first()
+        session.delete(link)
+        return True
     
     def __enter__(self):
         return self
--- football_rating_bot/football_rating_bot.py
+++ football_rating_bot/football_rating_bot.py
@@ -1,10 +1,13 @@
 from .football_database import FootballDatabase
 from .inline_prompt import InlinePrompt
+from football_rating.matchday import DEFAULT_ELO
 
 from telegram import Update, InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import Application, CallbackContext, ContextTypes
-from telegram.ext import CallbackQueryHandler, ChosenInlineResultHandler, InlineQueryHandler
+from telegram.ext import CallbackQueryHandler, ChosenInlineResultHandler, InlineQueryHandler, MessageHandler, filters
+from typing import List
 
+import json
 import logging
 import telegram
 import uuid
@@ -29,22 +32,8 @@ class FootballRatingBot:
             .write_timeout(30) \
             .build()
         
-    async def event_button(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        query = update.callback_query
-        username = query.from_user.username
-        logger.debug(f'event_button: data={query.data}, username={username}')
-        if not username:
-            return
-        data = query.data
-        try:
-            if data != 'event_button_0':
-                return
-            new_text = query.message + '\nТесто'
-            await query.edit_message_text(new_text)
-        except telegram.error.BadRequest as e:
-            logger.debug(f'Bad request: {str(e)}')
-        except KeyError as e:
-            logger.debug('No "id" in callback data')
+    async def action_button(self, data: dict, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        pass
         
     async def chosen_inline_result(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         chosen_result = update.chosen_inline_result
@@ -53,12 +42,29 @@ class FootballRatingBot:
         message_id = chosen_result.inline_message_id
         event_name = context.user_data.get('event_name')
         logger.debug(f'chosen_inline_result: result_id={result_id}, user_id={user_id}, message={message_id}')
+        event_id = uuid.uuid4()
         self.db.add_event(
-            event_id=uuid.UUID(result_id),
+            event_id=event_id,
             owner_id=user_id,
+            event_title=event_name
+        )
+        self.db.add_event_message(
+            inline_id=uuid.UUID(result_id),
             message_id=message_id,
-            event_name=event_name
+            event_id=event_id
         )        
+        
+    async def event_button(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        query = update.callback_query
+        data = json.loads(query.data)
+        buttons = [
+            self.participate_button,
+            self.action_button
+        ]
+        try:
+            await buttons[data['id']](data, update, context)
+        except IndexError as e:
+            logger.debug(f'Некорректная data в кнопке: {data}')
 
     async def inline_query(self, update: Update, context: CallbackContext):
         query = update.inline_query.query
@@ -68,15 +74,7 @@ class FootballRatingBot:
         logger.debug(f'Получен inline-запрос: {query} от {update.inline_query.from_user.id}, id={id}')
         markup = None
         if prompt.check_data():
-            button_data = {
-                'join': {'name': 'event_button_0', 'caption': 'Участвовать / Отмена'},
-                'actions': {'name' : 'event_button_1', 'caption': 'Действия'}
-            }
-            keyboard = [
-                [InlineKeyboardButton(button_data[key]['caption'], callback_data=button_data[key]['name'])]
-                for key in button_data.keys()
-            ]
-            markup = InlineKeyboardMarkup(keyboard)
+            markup = self._create_markup(id)
             context.user_data['event_name'] = prompt.input['name']
 
         results = [
@@ -84,13 +82,49 @@ class FootballRatingBot:
                 id=id,
                 title=prompt.prompt(),
                 description=prompt.description(),
-                input_message_content=InputTextMessageContent(prompt.answer()),
-                reply_markup=markup
+                input_message_content=InputTextMessageContent(f'<i>{prompt.answer()}:</i>', parse_mode='HTML'),
+                reply_markup=markup,
             )
         ]
 
         await update.inline_query.answer(results, cache_time=0)
 
+    async def participate_button(self, data: dict, update: Update, context: ContextTypes.DEFAULT_TYPE):        
+        query = update.callback_query
+        username = query.from_user.username
+        user_id = query.from_user.id
+        try:
+            inline_id = uuid.UUID(data['inline_id'])
+            event_id = self.db.get_event_id(inline_id)
+            event = self.db.get_event(event_id)
+            player_id = self.db.get_player_id(user_id)
+            # TODO: set elo for player
+            if not player_id:
+                player_id = self.db.add_player(username, user_id, DEFAULT_ELO)
+            # TODO: check max number
+            if self.db.is_player_registered(player_id, event_id):
+                self.db.unregister_player(player_id, event_id)
+            else:
+                self.db.register_player(player_id, event_id)
+                
+            players = self.db.registered_players(event_id)
+            message = self._create_text(event, players)
+            message_ids = self.db.get_messages(event_id)
+            for id in message_ids:
+                try:
+                    await context.bot.edit_message_text(
+                        inline_message_id=id,
+                        text=message,
+                        reply_markup=self._create_markup(str(inline_id)),
+                        parse_mode='HTML'
+                    )
+                except Exception as e:
+                    logger.debug(f'Не удалось отредактировать сообщение: {id}')                
+        except KeyError as e:
+            logger.debug(f'Некорректная data в кнопке: {data}')
+        except Exception as e:
+            logger.debug(f'Ошибка: {str(e)}')
+    
     def run(self):
         self.application.add_handler(InlineQueryHandler(self.inline_query))
         self.application.add_handler(ChosenInlineResultHandler(self.chosen_inline_result))
@@ -98,3 +132,27 @@ class FootballRatingBot:
 
         allowed_updates=['message', 'inline_query', 'chosen_inline_result', 'callback_query']
         self.application.run_polling(poll_interval=2, allowed_updates=allowed_updates)
+
+    def _create_markup(self, id: str) -> InlineKeyboardMarkup:
+        button_data = {
+            'join': {'caption': 'Участвовать / Отмена'},
+            'actions': {'caption': 'Действия'}
+        }
+        for i, button in enumerate(button_data):
+            button_data[button]['data'] = json.dumps({'id': i, 'inline_id': id})
+
+        keyboard = [
+            [InlineKeyboardButton(button_data[key]['caption'], callback_data=button_data[key]['data'])]
+            for key in button_data.keys()
+        ]
+        return InlineKeyboardMarkup(keyboard)
+
+    
+    def _create_text(self, event: dict, players: List[dict] | None) -> str:
+        title = f"<i>{event['event_title']}:</i>"
+        if not players:
+            return title
+        message = f'{title}\n'
+        names = [player['player_name'] for player in players]
+        message += '\n'.join(names)
+        return message
