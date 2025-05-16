198dc879746741584025becfb85a5f08d50a9a3e
sharrowkyn <abolginov@gmail.com>
2025-05-16 09:39:47 +0300

    rework bot to easy variant

--- football_rating_bot/football_database.py
+++ football_rating_bot/football_database.py
@@ -4,11 +4,11 @@ import uuid
 from enum import IntEnum, unique
 from functools import wraps
 
-from sqlalchemy import create_engine, Column, UUID, String, Integer, ForeignKey, func
+from sqlalchemy import create_engine, Column, UUID, String, Integer, ForeignKey, func, exists, desc
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session, relationship
 
-from typing import Callable, List, Tuple
+from typing import Callable, List, Tuple, Dict
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -18,43 +18,35 @@ logger = logging.getLogger(__name__)
 
 Base = declarative_base()
 
-class Event(Base):
-    __tablename__ = 'events'
-    event_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    owner_id = Column(Integer)
-    event_title = Column(String)
+class Owner(Base):
+    __tablename__ = 'owners'
+    owner_id = Column(Integer, primary_key=True) # telegram id
+    active_id = Column(Integer, default=0)
 
-    players = relationship('EventPlayer', back_populates='event')
-    messages = relationship('Message', back_populates='event')
+class Group(Base):
+    __tablename__ ='groups'
+    group_id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
+    owner_id = Column(Integer, ForeignKey('owners.id'))
 
 class Player(Base):
     __tablename__ = 'players'
-    player_id = Column(UUID(as_uuid=True), primary_key=True)
-    telegram_id = Column(Integer, default=0)
-    host_id = Column(Integer, default=0)
-    player_name = Column(String, default='')
-    elo = Column(Integer, default=1250)
-    matches = Column(Integer, default=0)
+    player_id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
+    elo = Column(Integer)
+    owner_id = Column(Integer, ForeignKey('owners.id'))    
+    group_id = Column(Integer, ForeignKey('groups.id'))    
 
-    events = relationship('EventPlayer', back_populates='player')
+class JoinUser(Base):
+    __tablename__ = 'joined'
+    user_id = Column(Integer, primary_key=True)
+    owner_id = Column(Integer)
+    role = Column(Integer)
 
-class Message(Base):
-    __tablename__ = 'messages'
-    inline_id = Column(UUID(as_uuid=True), primary_key=True)
-    message_id = Column(String, unique=True)    
-    event_id = Column(UUID(as_uuid=True), ForeignKey('events.event_id'))
-
-    event = relationship('Event', back_populates='messages')
-
-class EventPlayer(Base):
-    __tablename__ = 'event_players'
-    player_id = Column(UUID, ForeignKey('players.player_id'), primary_key=True)
-    event_id = Column(UUID, ForeignKey('events.event_id'), primary_key=True)
-    player_name = Column(String)
-    register_type = Column(Integer, default=0)
-    
-    event = relationship("Event", back_populates='players')
-    player = relationship("Player", back_populates='events')
+class User(Base):
+    __tablename__ = 'users'
+    user_id = Column(Integer, primary_key=True)
+    user_name = Column(String)
 
 # class Player(Base):
 #     __tablename__ = 'players'
@@ -72,11 +64,12 @@ def with_session(func: Callable):
     def wrapper(self, *args, **kwargs):
         with self.session() as session:
             try:
-                return func(self, session, *args, **kwargs)
+                return func(self, *args, session=session, **kwargs)
             except Exception as e:
                 logger.debug(f'Ошибка в {func.__name__}: {str(e)}')
                 logger.debug(args)
                 logger.debug(kwargs)
+                raise e
     return wrapper
 
 def with_commit(func: Callable):
@@ -92,233 +85,111 @@ def with_commit(func: Callable):
                 logger.debug(args)
                 logger.debug(kwargs)
                 session.rollback()
+                raise e
     return wrapper
 
-@unique
-class RegisterType(IntEnum):
-    MAIN=0,
-    RESERVE=1
+class RecordNotFound(LookupError):
+    pass
 
-    def other(self):
-        return RegisterType.MAIN if self == RegisterType.RESERVE else RegisterType.RESERVE
+class AdminRequired(LookupError):
+    pass
+
+@unique
+class UserRole(IntEnum):
+    ADMIN = 0,
+    USER = 1
 
 class FootballDatabase:
     def __init__(self, db_path: str):
         self.engine = create_engine(db_path)
         Base.metadata.create_all(self.engine)
         self.session = sessionmaker(bind=self.engine)
-
-    @with_commit
-    def add_event(self, event_id: uuid.UUID, owner_id: str, event_title: str, session=None) -> uuid.UUID | None:
-        new_event = Event(
-            event_id=event_id,
-            owner_id=owner_id,
-            event_title=event_title
-        )
-        session.add(new_event)
-        return event_id 
-    
-    def add_event_message(self, inline_id: uuid.UUID, message_id: uuid.UUID, event_id: uuid.UUID) -> uuid.UUID | None:
-        return self._add_event_message(inline_id, message_id, event_id)
-
-    def add_player(self, telegram_id: int, elo: int) -> uuid.UUID | None:
-        return self._add_player(telegram_id, elo)
-    
-    def add_invited_player(self, host_id: int, player_name: str, elo: int) -> uuid.UUID | None:
-        return self._add_invited_player(host_id, player_name, elo)
-    
-    def get_elo_range(self) -> Tuple[int, int]:
-        return self._get_elo_range()    
-    
-    def get_event(self, event_id: uuid.UUID) -> dict | None:
-        return self._get_event(event_id)
-    
-    def get_events(self, owner_id: int) -> List[dict] | None:
-        return self._get_events(owner_id)
-      
-    def get_event_by_inline(self, inline_id: uuid.UUID) -> uuid.UUID | None:
-        return self._get_event_by_inline(inline_id)
-    
-    def get_messages(self, event_id: uuid.UUID) -> List[str] | None:
-        return self._get_messages(event_id)    
-  
-    def get_player_id(self, telegram_id: int, host_id: int = 0, player_name: str = '') -> uuid.UUID | None:
-        return self._get_player_id(telegram_id, host_id, player_name)
-    
-    def is_player_registered(self, event_id: uuid.UUID, player_id: uuid.UUID) -> RegisterType | None:
-        return self._is_player_registered(event_id, player_id)
-    
-    # def is_player_registered(self, player_id, event_id) -> bool:
-    #     return self._is_player_registered(player_id, event_id)
-
-    def register_player(
-            self,
-            player_id: uuid.UUID,
-            event_id: uuid.UUID,
-            player_name: str,
-            register_as: RegisterType = RegisterType.MAIN     
-        ) -> bool | None:
-        return self._register_player(player_id, event_id, player_name, register_as)
-    
-    def registered_players(self, event_id: uuid.UUID) -> bool | None:
-        return self._registered_players(event_id)
-    
-    def remove_event(self, session: Session, event_id: uuid.UUID):
-        raise NotImplementedError('delete messages from table')
-        with self.session() as session:            
-            try:
-                logger.debug(f'Удаление из базы: event_id = {event_id}')
-                event = session.query(Event).filter(Event.evnet_id == event_id).first()
-                if not event:
-                    return
-                session.delete(event)
-                session.commit()
-                logger.debug('Commit')
-            except Exception as e:
-                session.rollback()
-                logger.debug('Rollback')
-
-    def unregister_player(self, player_id: uuid.UUID, event_id: uuid.UUID) -> bool | None:                
-        return self._unregister_player(player_id, event_id)
-    
-    def update_player(self, player_id: uuid.UUID, elo: int):
-        return self._update_player(player_id, elo)
-
-    # def update_event_text(self, event_id: uuid.UUID, new_text: str) -> None:
-    #     return self._update_event_text(event_id, new_text)    
-    
-    @with_commit
-    def _add_event_message(self, session: Session, inline_id: uuid.UUID, message_id: uuid.UUID, event_id: uuid.UUID) -> uuid.UUID | None:
-        new_message = Message(inline_id=inline_id, message_id=message_id, event_id=event_id)
-        session.add(new_message)
-        return inline_id    
-    
-    @with_commit
-    def _add_player(self, session: Session, telegram_id: int, elo: int) -> uuid.UUID | None:
-        player_id = uuid.uuid4()
-        new_player = Player(
-            player_id=player_id,
-            telegram_id=telegram_id,
-            elo=elo
-        )
-        session.add(new_player)
-        return player_id
-    
-    @with_commit
-    def _add_invited_player(self, session: Session, host_id: int, player_name: str, elo: int) -> uuid.UUID | None:
-        player_id = uuid.uuid4()
-        new_player = Player(
-            player_id=player_id,
-            host_id=host_id,
-            player_name=player_name,
-            elo=elo
-        )
-        session.add(new_player)
-        return player_id
-    
-    @with_session
-    def _get_elo_range(self, session: Session) -> Tuple[int, int]:
-        max_elo = int(session.query(func.max(Player.elo)).scalar() or 0)
-        min_elo = int(session.query(func.min(Player.elo)).scalar() or 0) 
-        return (min_elo, max_elo)
-    
-    @with_session
-    def _get_event(self, session: Session, event_id: uuid.UUID) -> dict | None:
-        event = session.query(Event).filter_by(event_id=event_id).first()
-        return model_to_dict(event)
-    
-    @with_session
-    def _get_events(self, session: Session, owner_id: int) -> List[dict] | None:
-        events = session.query(Event).filter_by(owner_id=owner_id).all()
-        return [model_to_dict(event) for event in events]
-    
-    @with_session
-    def _get_event_by_inline(self, session: Session, inline_id: uuid.UUID) -> uuid.UUID | None:
-        rec = session.query(Message).filter_by(inline_id=inline_id).first()
-        if not rec:
-            return None
-        return rec.event_id
-    
-    @with_session
-    def _get_messages(self, session: Session, event_id: uuid.UUID) -> List[str] | None:
-        event = session.query(Event).filter_by(event_id=event_id).first()        
-        return [{'inline_id': message.message_id, } for message in event.messages]
-    
-    @with_session
-    def _get_player_id(self, session: Session, telegram_id: int, host_id: int = 0, player_name: str = '') -> uuid.UUID | None:
-        if telegram_id:
-            rec = session.query(Player).filter_by(telegram_id=telegram_id).first()
-        else:
-            rec = session.query(Player).filter_by(host_id=host_id, player_name=player_name).first()
-        if not rec:
-            return None
-        return rec.player_id            
-    
-    @with_session
-    def _is_player_registered(self, session: Session, event_id: uuid.UUID, player_id: uuid.UUID) -> RegisterType | None:
-        rec = session.query(EventPlayer).filter_by(player_id=player_id, event_id=event_id).first()
-        if not rec:
-            return None
-        return rec.register_type
-    
-    @with_commit
-    def _register_player(
-            self,
-            session: Session,
-            player_id: uuid.UUID,
-            event_id: uuid.UUID,
-            player_name: str,
-            register_as: RegisterType
-        ) -> bool | None:
-        link = EventPlayer(
-            player_id=player_id,
-            event_id=event_id,
-            player_name=player_name,
-            register_type=register_as
-        )        
-        session.add(link)
-        return True
-    
-    @with_session
-    def _registered_players(self, session: Session, event_id: uuid.UUID) -> List[dict] | None:
-        event_players = session.query(EventPlayer).filter_by(event_id=event_id).order_by(EventPlayer.register_type).all()
-        if not event_players:
-            return None
-        return [
-            {**model_to_dict(player.player), "player_name": player.player_name}
-            for player in event_players
-        ]
-        # event = session.query(Event).filter_by(event_id=event_id).first()        
-        # return [{**model_to_dict(player.player), "player_name": player.player_name } for player in event.players]
-    
-    # @with_session
-    # def is_player_registered(self, session: Session, player_id, event_id) -> bool:
-    #     rec = session.query(EventPlayer).filter(
-    #         EventPlayer.event_id == event_id,
-    #         EventPlayer.player_id == player_id
-    #     ).first()
-    #     return bool(rec)
-
-    # @with_commit
-    # def _update_event_text(self, session: Session, event_id: uuid.UUID, new_text: str) -> None:
-    #     event = session.query(Event).filter(Event.event_id == event_id).first()
-    #     event.event_text = new_text
-    #     return new_text
-    
-    @with_commit
-    def _update_player(self, session: Session, player_id: uuid.UUID, elo: int):
-        player = session.query(Player).filter_by(player_id=player_id).first()
-        player.elo = elo
-        return elo
-
-    @with_commit
-    def _unregister_player(self, session: Session, player_id: uuid.UUID, event_id: uuid.UUID) -> bool | None:                
-        link = session.query(EventPlayer).filter_by(player_id=player_id, event_id=event_id).first()
-        session.delete(link)
-        return True
     
     def __enter__(self):
         return self
     
     def __exit__(self, exc_type, sxc_val, exc_tb):
         pass
+
+    def add_admin(self, owner_id: int, name: str, state: bool):
+        return self._add_admin(owner_id, name, state)
+    
+    def add_player(self, owner_id: int, name: str, elo: int):
+        return self._add_player(owner_id, name, elo)
+
+    def add_user(self, user_id: int, user_name: str):
+        return self._add_user(user_id, user_name)        
+
+    def get_ratings(self, owner_id: int):
+        return self._get_ratings(self, owner_id)
+
+    def select_group(self, owner_id: int, name: str):
+        return self._select_group(owner_id, name)
+
+    @with_commit
+    def _add_admin(self, owner_id: int, name: str, state: bool, session: Session):
+        user = session.query(User).filter_by(user_name=name).first()
+        if not user:
+            raise RecordNotFound()
+        user_id = user.user_id
+        user = session.query(JoinUser).filter_by(user_id=user_id, owner_id=owner_id).first()
+        if not user:
+            raise RecordNotFound()
+        if state:
+            user.role = UserRole.ADMIN
+        else:
+            user.role = UserRole.USER
+        
+        
+
+    @with_commit
+    def _add_player(self, owner_id: int, name: str, elo: int, session: Session):
+        owner_id = self._change_user(owner_id, UserRole.ADMIN, session)
+        owner = session.query(Owner).filter_by(id=owner_id).first()
+        if not owner:
+            raise RecordNotFound()
+        new_player = Player(name=name, elo=elo, owner_id=owner_id, group_id=owner.active_id)
+        session.add(new_player)
+
+    @with_commit
+    def _add_user(self, user_id: int, user_name: str, session: Session):
+        user = session.query(User).filter_by(user_id=user_id).first()
+        if user:
+            user.user_name = user_name
+        else:
+            new_user = User(user_id=user_id, user_name=user_name)
+            session.add(new_user)
+    
+    @with_session
+    def _change_user(self, user_id: int, required_role: UserRole, session: Session) -> int:
+        # проверяем, присоединен ли user к другому
+        user = session.query(JoinUser).filter_by(user_id=user_id).first()
+        if not user:
+            return user_id
+        if required_role == UserRole.ADMIN and user.role == UserRole.USER:
+            raise AdminRequired()
+        return user.owner_id
+    
+    @with_session
+    def _get_ratings(self, owner_id: int, session: Session) -> Dict[str, int]:
+        owner_id = self._change_user(owner_id, UserRole.USER, session)
+        players = (
+            session.query(Player)
+            .join(Owner)
+            .filter(Player.owner_id == owner_id, Player.group_id == Owner.active_id)
+        ).order_by(desc(Player.elo)).all()
+        return [model_to_dict(player) for player in players]
+
+
+    @with_commit
+    def _select_group(self, owner_id: int, name: str, session: Session):
+        owner_id = self._change_user(owner_id, UserRole.ADMIN, session)
+                    
+        if not session.query(exists().where(Owner.owner_id == owner_id)):
+            new_owner = Owner(id=owner_id)
+            session.add(new_owner)
+        if not session.query(exists().where(Group.name == name)):
+            new_group = Group(name=name, owner_id=owner_id)
+            session.add(new_group)
+        session.commit()
+        new_owner.active_id = new_group.id
--- football_rating_bot/football_rating_bot.py
+++ football_rating_bot/football_rating_bot.py
@@ -1,16 +1,13 @@
-from .bot_automat import BotAutomat, CommandState
-from .football_database import FootballDatabase, RegisterType
-from .inline_prompt import InlinePrompt
+from .football_database import FootballDatabase, RecordNotFound, AdminRequired
 from football_rating.matchday import DEFAULT_ELO
 
-from telegram import Update, InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardMarkup, InlineKeyboardButton
-from telegram.ext import Application, CallbackContext, ContextTypes, filters
-from telegram.ext import CallbackQueryHandler, ChosenInlineResultHandler, InlineQueryHandler, CommandHandler, MessageHandler
+from telegram import Update, InputFile
+from telegram.ext import Application, ContextTypes
 from typing import List
 
+import io
 import logging
-import telegram
-import uuid
+import pandas as pd
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(lineno)d - %(message)s',
@@ -21,7 +18,17 @@ logging.getLogger('httpcore').setLevel(logging.WARNING)
 logging.getLogger('httpx').setLevel(logging.WARNING)
 logging.getLogger('telegram.ext').setLevel(logging.WARNING)
 
+class ArgumentLengthException(Exception):
+    pass
+
 class FootballRatingBot:
+    admin_error_text = 'Необходимы права администратора.'
+    too_long_error_text = 'Слишком длинное названия/имя (максимум 64 символа).'
+    internal_error_text = 'Произошла внутренняя ошибка.'
+    wrong_argument_text = 'Неверный аргумент.'    
+    max_len = 64
+    max_elo = 3000
+
     def __init__(self, token, db_path):
         self.token = token
         self.db = FootballDatabase(db_path)
@@ -30,19 +37,104 @@ class FootballRatingBot:
             .token(self.token) \
             .build()
         # TODO: garbage collector
-        self.automats = {}
+
+    async def add(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        try:
+            args = update.message.text.split(' ')[1:]
+            name = ' '.join(args[:-1])
+            if len(name) > self.max_len:
+                raise ArgumentLengthException
+            elo = int(args[-1])
+            self.db.add_player(update.effective_user.id, name, elo)
+        except ArgumentLengthException:
+            await update.message.reply_text(self.too_long_error_text)                        
+        except AdminRequired:
+            await update.message.reply_text(self.admin_error_text)            
+        except ValueError:
+            await update.message.reply_text(self.wrong_argument_text)
+        except Exception as e:
+            await update.message.reply_text(self.internal_error_text)
         
+    async def admin(self, update: Update, context: ContextTypes.DEFAULT_TYPE)            :
+        try:
+            args = self._get_argument(update.message.text).split(' ')
+            state = args[0].lower()
+            if state != 'on' or state != 'off':
+                raise ValueError()
+            state = state == 'on'
+            name = args[1]
+            if not name.startswith('@'):
+                raise ValueError()
+            name = name[1:]
+            self.db.add_admin(update.effective_user.id, name, state)
+        except (ValueError, IndexError):
+            await update.message.reply_text(self.wrong_argument_text)
+        except RecordNotFound:
+            await update.message.reply_text('Пользователь не найден. Попросите его присоединиться.')
+        except Exception as e:
+            await update.message.reply_text(self.internal_error_text)
+
+    async def export(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        try:
+            ratings = self.db.get_ratings(update.effective_user.id)
+            df = pd.DataFrame(ratings)
+            csv_buffer = io.StringIO()
+            df.to_csv(csv_buffer, index=False)
+            csv_buffer.seek(0)
+            update.message.reply_document(
+                document=InputFile(csv_buffer, filename='players_elo.csv'),
+                caption='Рейтинг игроков'
+            )
+        except Exception as e:
+            await update.message.reply_text(self.internal_error_text)
+
+    async def help(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        text: str = (
+            '<b>Список команд:</b>\n'
+            '/add [Имя] - добавить участника\n'
+            '/admin [on|off] [@user] - дать @user права редактирования своих таблиц\n'
+            '/export - выгрузить таблицу в csv'
+            '/groups - список групп\n'
+            '/help - список команд\n'
+            '/join [on|off] [@user] - присоединиться к таблицам @user\n'
+            '/rename [Имя] - [Имя] - переименовать участника\n'
+            '/results [Результаты] - загрузить результаты\n'
+            '/split [Количество команд] [Список участников] - разбить на команды\n'
+            '/start [Имя группы] - запуск или переключение между группами участников\n'
+        )
+        await update.message.reply_text(text)
     
     def run(self):
+        self.application.add_handler(CommandHandler("add", self.add))
+        self.application.add_handler(CommandHandler("help", self.help))
         self.application.add_handler(CommandHandler("start", self.start))
 
         allowed_updates=['message']
         self.application.run_polling(poll_interval=2, allowed_updates=allowed_updates)
 
     async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        user_id = update.message.from_user.id
-        try:            
-            automat = self.automats[user_id] = BotAutomat(self.db, user_id)
-            await automat.prompt(update, context)
-        except Exception as e:
-            logger.debug(f'Ошибка: {str(e)}') 
+        user = update.effective_user
+        try:
+            self.db.add_user(user.id, user.username)
+            group = self._get_argument(update.message.text, self.max_len)
+            if not group:
+                group = 'Default'
+            self.db.select_group(user.id, group)                
+            await update.message.reply_text(f'Успешно переключились на группу: {group}')
+        except ArgumentLengthException:
+            await update.message.reply_text(self.too_long_error_text)            
+        except AdminRequired:
+            await update.message.reply_text(self.admin_error_text)
+        except Exception:
+            await update.message.reply_text(self.internal_error_text)
+
+    def _get_argument(self, text: str, max_len: int | None = None):
+        index = text.find(' ')
+        if index == -1:
+            return ''
+        arg = text[index + 1:]
+        if max_len and len(arg) > max_len:
+            raise ArgumentLengthException()
+        return arg
+
+        
